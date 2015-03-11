4e5c240bd61c038f251c841edc942d68d3ca0f08
Gautam Madaan <gautam.madaan@gmail.com>
2015-03-10 21:13:31 -0700

    Initial Commit

---
+++ DreamMeetup/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DreamMeetup
+//
+//  Created by Gautam Madaan on 3/10/15.
+//  Copyright (c) 2015 Gautam Madaan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DreamMeetup/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DreamMeetup
+//
+//  Created by Gautam Madaan on 3/10/15.
+//  Copyright (c) 2015 Gautam Madaan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
